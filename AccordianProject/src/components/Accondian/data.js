3949d5ed9c47206aa0c27b91399cf2964d3ffc1f
ram muwel <shrirammuwel02017@gmail.com>
2024-12-26 17:19:29 +0530

    initial commit

---
+++ AccordianProject/src/components/Accondian/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+      id: "1",
+      question: "What is JavaScript?",
+      answer: "JavaScript is a scripting language. It is different from Java language. It is object-based, lightweight, cross-platform translated language. It is widely used for client-side validation. The JavaScript Translator (embedded in the browser) is responsible for translating the JavaScript code for the web browser."
+    },
+    {
+      id: "2",
+      question: " Who developed JavaScript, and what was the first name of JavaScript?",
+      answer: "JavaScript was developed by Brendan Eich, who was a Netscape programmer. Brendan Eich developed this new scripting language in just ten days in the year September 1995. At the time of its launch, JavaScript was initially called Mocha. After that, it was called Live Script and later known as JavaScript."
+    },
+    
+    {
+      id: "3",
+      question: "List some of the advantages of JavaScript?",
+      answer: "Some of the advantages of JavaScript are: 1. Server interaction is less, 2. Feedback to the visitors is immediate, 3.Interactivity is high 4. Interfaces are richer."
+    },
+    {
+      id: "4",
+      question: "In JavaScript what is an argument object?",
+      answer: "The variables of JavaScript represent the arguments that are passed to a function."
+    },
+    {
+      id: "5",
+      question: "Define closure?",
+      answer: "In JavaScript, we need closures when a variable which is defined outside the scope in reference is accessed from some inner scope."
+    },
+];
